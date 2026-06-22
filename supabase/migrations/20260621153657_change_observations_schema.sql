3016966ac75b3f5dda644678374ca80af2bacdc1
NicT5 <nicctang@gmail.com>
2026-06-22 23:15:40 +0800

    Update sql script

--- supabase/migrations/20260621153657_change_observations_schema.sql
+++ supabase/migrations/20260621153657_change_observations_schema.sql
@@ -1,12 +1,11 @@
 DELETE FROM public.observations o1
 WHERE o1.id NOT IN (
-  SELECT MIN(o2.id)
+  SELECT DISTINCT ON (o2.slot_id) o2.id
   FROM public.observations o2
-  GROUP BY o2.slot_id
+  ORDER BY o2.slot_id, o2.id
 );
 
-ALTER TABLE public.observations DROP COLUMN 'user_id';
-ALTER TABLE public.observations ADD COLUMN 'submitted_by';
+ALTER TABLE public.observations DROP COLUMN user_id;
 
 ALTER TABLE public.observations
 ADD CONSTRAINT observations_slot_id_unique UNIQUE (slot_id);
